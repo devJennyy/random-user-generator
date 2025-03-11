8b256d61b825a4b1e967a3bc293fea5362724821
Jenny Pieloor <devjenny.official@gmail.com>
2025-03-11 23:01:54 +0800

    Improve responsiveness.

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -15,18 +15,18 @@ function Switch({
       onCheckedChange={setIsChecked}
       data-slot="switch"
       className={cn(
-        "relative cursor-pointer peer focus-visible:border-ring focus-visible:ring-ring/50 inline-flex h-8 w-20 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "relative cursor-pointer peer focus-visible:border-ring focus-visible:ring-ring/50 inline-flex sm:h-8 sm:w-20 h-7 w-[70px] shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
         isChecked ? "bg-white" : "bg-darkGray",
         className
       )}
       {...props}
     >
       {isChecked ? (
-        <span className="absolute left-3 text-sm font-medium text-darkGray">
+        <span className="absolute sm:left-3 left-2 text-sm font-medium text-darkGray">
           Dark
         </span>
       ) : (
-        <span className="absolute right-3 text-sm font-medium text-white">
+        <span className="absolute sm:right-3 right-2 text-sm font-medium text-white">
           Light
         </span>
       )}
@@ -34,9 +34,9 @@ function Switch({
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
         className={cn(
-          "cursor-pointer bg-background pointer-events-none block size-[26px] rounded-full ring-0 shadow-lg transition-transform",
+          "cursor-pointer bg-background pointer-events-none block sm:size-[26px] size-[22px] rounded-full ring-0 shadow-lg transition-transform",
           isChecked
-            ? "translate-x-[50px] bg-primary border-white"
+            ? "sm:translate-x-[50px] translate-x-[44px] bg-primary border-white"
             : "translate-x-[2px] bg-primary border-white"
         )}
       />
--- src/page/Hero.tsx
+++ src/page/Hero.tsx
@@ -4,26 +4,26 @@ import { profileDetails } from "@/constants/profileData";
 const Hero = () => {
   return (
     <div className="relative w-full flex flex-col items-center">
-      <div className="w-full h-[361px] bg-primary flex flex-col justify-center items-center gap-8">
-        <p className="text-6xl font-medium text-white">Random User Generator</p>
+      <div className="w-full lg:h-[361px] h-[280px] bg-primary flex flex-col lg:justify-center items-center sm:gap-8 gap-5 lg:pt-0 sm:pt-19 pt-11">
+        <p className="lg:text-6xl sm:text-5xl text-[30px] font-medium text-white leading-9">
+          Random User <br className="sm:hidden" />
+          Generator
+        </p>
 
         <Switch />
       </div>
-      <div className="absolute top-[17rem] w-full max-w-[750px] flex flex-col items-center gap-6">
-        <div className="w-[205px] h-[205px] bg-slate-400 border-5 border-white rounded-3xl"></div>
-        <p className="text-[40px] font-bold">Jane Smith Doe</p>
-        <div className="flex gap-12">
+      <div className="absolute lg:top-[17rem] sm:top-[14rem] top-[12rem] w-full max-w-[750px] flex flex-col items-center gap-6">
+        <div className="sm:w-[205px] sm:h-[205px] w-[170px] h-[170px] bg-slate-400 border-5 border-white rounded-3xl"></div>
+        <p className="sm:text-[40px] text-3xl font-bold">Jane Smith Doe</p>
+        <div className="flex sm:gap-12 gap-4">
           {profileDetails?.map((item) => {
             const IconComponent = item.icon;
             return (
               <div className="group cursor-pointer flex flex-col justify-center items-center gap-2">
-                <div className="w-[65px] h-[65px] flex justify-center items-center rounded-3xl transition-default bg-transparent group-hover:bg-primary">
-                  <IconComponent
-                    size={24}
-                    className="text-primary/60 group-hover:text-white transition-default"
-                  />
+                <div className="sm:w-[65px] sm:h-[65px] w-[50px] h-[50px] flex justify-center items-center sm:rounded-3xl rounded-2xl transition-default bg-transparent group-hover:bg-primary">
+                  <IconComponent className="sm:text-[24px] text-[20px] text-primary/60 group-hover:text-white transition-default" />
                 </div>
-                <p className="font-medium text-primary/60 group-hover:text-primary transition-default">
+                <p className="sm:text-[16px] text-sm font-medium text-primary/60 group-hover:text-primary transition-default">
                   {item.label}
                 </p>
               </div>
@@ -31,7 +31,7 @@ const Hero = () => {
           })}
         </div>
 
-        <div className="flex gap-3 !mt-20">
+        <div className="flex gap-3 !mt-10">
           <button className="px-6 py-2 border rounded-lg font-bold cursor-pointer">
             Prev
           </button>
