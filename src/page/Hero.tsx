f7e12456651a0086811350347175e48e0a526461
Jenny Pieloor <devjenny.official@gmail.com>
2025-03-13 21:35:39 +0800

    Add active state.

--- src/page/Hero.tsx
+++ src/page/Hero.tsx
@@ -1,9 +1,14 @@
 import { Switch } from "@/components/ui/switch";
-import { profileDetails } from "@/constants/profileData";
 import axios from "axios";
 import { useState } from "react";
+import { BiSolidPhoneCall } from "react-icons/bi";
+import { CgUserlane } from "react-icons/cg";
+import { IoChatboxEllipses } from "react-icons/io5";
+import { MdDateRange } from "react-icons/md";
+import { TbLocationFilled } from "react-icons/tb";
 
 const Hero = () => {
+  const [active, setActive] = useState("nameTab")
   const [person, setPerson] = useState({
     image: null,
     firstName: null,
@@ -24,6 +29,7 @@ const Hero = () => {
         console.log(err);
       });
   };
+  
   return (
     <div className="relative w-full flex flex-col items-center">
       <div className="w-full lg:h-[361px] h-[280px] bg-primary flex flex-col lg:justify-center items-center lg:gap-10 sm:gap-8 gap-5 lg:pt-0 sm:pt-19 pt-11">
@@ -52,23 +58,42 @@ const Hero = () => {
         ) : (
           <p className="sm:text-[40px] text-3xl font-bold">Jane Doe</p>
         )}
+
         <div className="flex sm:gap-12 gap-4">
-          {profileDetails?.map((item, index) => {
-            const IconComponent = item.icon;
-            return (
-              <div
-                key={index}
-                className="group cursor-pointer flex flex-col justify-center items-center gap-2"
-              >
-                <div className="sm:w-[65px] sm:h-[65px] w-[50px] h-[50px] flex justify-center items-center sm:rounded-3xl rounded-2xl transition-default bg-transparent group-hover:bg-primary group-active:bg-primary">
-                  <IconComponent className="sm:text-[24px] text-[20px] text-primary/60 group-hover:text-white group-active:text-white transition-default" />
-                </div>
-                <p className="sm:text-[16px] text-sm font-medium text-primary/60 group-hover:text-primary group-active:text-primary transition-default">
-                  {item.label}
-                </p>
-              </div>
-            );
-          })}
+          <button onClick={() => setActive("nameTab")} className="cursor-pointer flex flex-col justify-center items-center gap-2">
+            <div className={`transition-default sm:w-[65px] sm:h-[65px] w-[50px] h-[50px] flex justify-center items-center sm:rounded-3xl rounded-2xl ${active === "nameTab" ? "bg-primary text-white" : "bg-transparent text-primary"}`}>
+              <CgUserlane className="sm:text-[24px] text-[20px]" />
+            </div>
+            <p className={`sm:text-[16px] text-sm font-medium ${active === "nameTab" ? "text-primary" : "text-primary/60"}`}>Name</p>
+          </button>
+        
+          <button onClick={() => setActive("emailTab")} className="cursor-pointer flex flex-col justify-center items-center gap-2">
+            <div className={`transition-default sm:w-[65px] sm:h-[65px] w-[50px] h-[50px] flex justify-center items-center sm:rounded-3xl rounded-2xl ${active === "emailTab" ? "bg-primary text-white" : "bg-transparent text-primary"}`}>
+              <IoChatboxEllipses className="sm:text-[24px] text-[20px]" />
+            </div>
+            <p className={`transition-default sm:text-[16px] text-sm font-medium ${active === "emailTab" ? "text-primary" : "text-primary/60"}`}>Email</p>
+          </button>
+
+          <button onClick={() => setActive("birthdateTab")} className="cursor-pointer flex flex-col justify-center items-center gap-2">
+            <div className={`transition-default sm:w-[65px] sm:h-[65px] w-[50px] h-[50px] flex justify-center items-center sm:rounded-3xl rounded-2xl ${active === "birthdateTab" ? "bg-primary text-white" : "bg-transparent text-primary"}`}>
+              <MdDateRange className="sm:text-[24px] text-[20px]" />
+            </div>
+            <p className={`transition-default sm:text-[16px] text-sm font-medium ${active === "birthdateTab" ? "text-primary" : "text-primary/60"}`}>BirthDate</p>
+          </button>
+
+          <button onClick={() => setActive("locationTab")} className="cursor-pointer flex flex-col justify-center items-center gap-2">
+            <div className={`transition-default sm:w-[65px] sm:h-[65px] w-[50px] h-[50px] flex justify-center items-center sm:rounded-3xl rounded-2xl ${active === "locationTab" ? "bg-primary text-white" : "bg-transparent text-primary"}`}>
+              <TbLocationFilled className="sm:text-[24px] text-[20px]" />
+            </div>
+            <p className={`transition-default sm:text-[16px] text-sm font-medium ${active === "locationTab" ? "text-primary" : "text-primary/60"}`}>Location</p>
+          </button>
+
+          <button onClick={() => setActive("contactTab")} className="cursor-pointer flex flex-col justify-center items-center gap-2">
+            <div className={`transition-default sm:w-[65px] sm:h-[65px] w-[50px] h-[50px] flex justify-center items-center sm:rounded-3xl rounded-2xl ${active === "contactTab" ? "bg-primary text-white" : "bg-transparent text-primary"}`}>
+              <BiSolidPhoneCall className="sm:text-[24px] text-[20px]" />
+            </div>
+            <p className={`transition-default sm:text-[16px] text-sm font-medium ${active === "contactTab" ? "text-primary" : "text-primary/60"}`}>Contact</p>
+          </button>
         </div>
 
         <button
